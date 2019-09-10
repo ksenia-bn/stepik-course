1f0549be6bdc031029c0e2f48d8e212af55707d6
bkn <bychkova.kn@mail.ru>
2019-09-10 13:26:58 +0700

    lesson3.6_step9: добавила assert в тестовое задание

--- test_items.py
+++ test_items.py
@@ -1,10 +1,13 @@
 import time
+from selenium import webdriver
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_add_basket(browser):
     browser.get(link)
     time.sleep(2)
-    browser.find_element_by_css_selector(".btn-add-to-basket")  
+    browser.find_element_by_css_selector(".btn-add-to-basket")    
+    assert True, "Test is passed!"
+    
 
 
