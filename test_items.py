1796960d3a06815697062bf92aea83826d7c12fb
bkn <bychkova.kn@mail.ru>
2019-09-10 14:06:48 +0700

    lesson3.6_step9: добавила assert в тестовое задание

--- test_items.py
+++ test_items.py
@@ -5,9 +5,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_add_basket(browser):
     browser.get(link)
-    time.sleep(2)
-    browser.find_element_by_css_selector(".btn-add-to-basket")    
-    assert True, "Test is passed!"
-    
+    #time.sleep(10)    
+    assert browser.find_element_by_class_name("btn-add-to-basket")
 
 
